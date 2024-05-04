c642d6682bad92e703aa5c7aef147b6eaa56cb67
Jesus1102 <jesus60302@gmail.com>
2024-05-04 11:27:03 -0500

    Cree la struct Tarea

--- Creador.cpp
+++ Creador.cpp
@@ -14,20 +14,43 @@ struct Proyecto{
 
 };
 
+struct Tarea{
+
+    string nombreTarea;
+    string estadoTarea;
+    string prioridadTarea;
+    string responsableTarea;
+    string fechaLimiteTarea;
+    string resumenTarea;
+
+}; 
+
+
+
 class Creador{
 
     public:
-    Proyecto proyecto;
+    Proyecto pro;
 
     void crearProyecto(){
 
+        cout << "Ingrese el nombre del Proyecto: " << endl;
+        cin >> pro.nombreProy;
 
+        cout << "Ingrese el propietario del Proytecto: " << endl;
+        cin >> pro.propietarioProy;
+
+        cout << "Ingrese el estado del Proyecto:" << endl;
+        cin >> pro.estadoProy;
+
+        cout << "Ingrese la descripcion del Proyecto: " << endl;
+        cin >> pro.descripcionProy;
 
     }
 
     void crearTarea(){
 
-
+        cout << "";
 
 
     }
