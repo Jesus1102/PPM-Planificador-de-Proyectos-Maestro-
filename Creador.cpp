081c0c86caf9a4484a4567d4d859b276f9fe16b5
Jesus1102 <jesus60302@gmail.com>
2024-05-07 09:49:57 -0500

    Cree la funcion prioTarea

--- Creador.cpp
+++ Creador.cpp
@@ -3,6 +3,9 @@
 #include <stack>
 #include <map>
 #include <set>
+#include <windows.h>
+
+#define color SetConsoleTextAttribute
 
 using namespace std;
 
@@ -35,6 +38,8 @@ class Creador{
     set<Proyecto> proyecGenerales;
     set <Tarea> tareasGenerales;
     map <string, set<Tarea>> proyecTareas;
+    int color1 [4] = {4,12,14,10};
+
     
 
     void crearProyecto(){
@@ -63,8 +68,7 @@ class Creador{
         cout << "Ingrese el estado de la Tarea: " << endl;
         cin >> task.estadoTarea;
 
-        cout << "Ingrese la prioridad de la Tarea: " << endl;
-        cin >> task.prioridadTarea;
+        prioTarea();
 
         cout << "Ingrese el responsable de la Tarea: " << endl;
         cin >> task.responsableTarea;
@@ -76,6 +80,28 @@ class Creador{
         cin >> task.resumenTarea;
 
         tareasGenerales.insert(task);
+
+    }
+
+    void prioTarea(){
+
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        string prioridad [4] = {"Urgente","Alta","Media","Baja"};
+        int opc;
+
+        for(int i = 0; i < 4; i++){
+
+            color(hConsole, color1[i]);
+            cout << to_string(i+1) << prioridad[i] << endl;
+
+        }
+
+        cout << "Ingrese la prioridad de la tarea: " << endl;
+        cin >> opc;
+        opc--;
+
+        task.prioridadTarea = prioridad[opc];
 
     }
 
